e745ae8d79055c7385bdc604407628312813ee01
Esther Mollica <bionicfemme@me.com>
2017-08-28 12:42:24 -0400

    first commit

---
+++ client/config.js
@@ -0,0 +1,8 @@
+export default {
+  bucket: {
+    slug: 'your-bucket-slug', // add your Bucket slug here
+    type_slug: 'tasks',
+    read_key: '', // add read key if added to Cosmic JS > Your Bucket > Settings
+    write_key: '' // add write key if added to Cosmic JS > Your Bucket > Settings
+  }
+}
